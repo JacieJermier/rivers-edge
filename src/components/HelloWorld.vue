aa2fb4f5f24e8542288e1d58d38774d322da3683
Jacie <jacie.jermier@bucks.com>
2023-09-11 09:07:26 -0500

    add rivers edge lamding

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,38 +1,173 @@
-<script setup lang="ts">
-import { ref } from 'vue'
-
-defineProps<{ msg: string }>()
-
-const count = ref(0)
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <h1>{{ msg }}</h1>
+  <div class="layout">
+    <div class="video-container">
+      <video autoplay mute playsinline loop>
+        <source src="../assets/videos/rivers-edge-video.mp4" />
+      </video>
+    </div>
 
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
+    <div class="info-container">
+      <div class="text-container">
+        <h1>Rivers Edge Racing <br />Now Open</h1>
+        <p>
+          We are thrilled to announce Rivers Edge Racing has now opened it’s
+          doors! Our passion for racing has driven us to curate a selection of
+          unique bikes and racing gear, ensuring that every enthusiast finds
+          something that resonates with their racing spirit. We invite you to
+          experience this firsthand and explore our diverse range.
+        </p>
+        <p>
+          Located in: Shops at Fallen Timbers 3100 Main St #1200, Maumee, OH
+          43537
+        </p>
+        <div class="button-container">
+          <a
+            class="cta-button"
+            href="https://www.google.com/maps/dir//3100+Main+St+%231200,+Maumee,+OH+43537/@41.5474407,-83.788498,12z/data=!4m8!4m7!1m0!1m5!1m1!1s0x883c71026ce0de55:0xc74663ce8f2566de!2m2!1d-83.7060972!2d41.5474701?entry=ttu"
+            style="margin-right: 0.5em"
+          >
+            Directions
+          </a>
+          <a
+            class="cta-button"
+            href="tel:+14197753343"
+            style="margin-left: 0.5em"
+          >
+            Contact
+          </a>
+        </div>
+      </div>
+      <div class="image-container">
+        <img
+          src="../assets//images/RER-landing-image.jpeg"
+          alt="three images of motorcycles"
+        />
+      </div>
+    </div>
+    <div class="social-icons">
+      <a href="https://www.facebook.com/profile.php?id=100090918243835"
+        ><img src="../assets/images/facebook.svg" alt="Facebook"
+      /></a>
+      <a href="https://www.instagram.com/rivers_edge_racing/"
+        ><img src="../assets/images/instagram.svg" alt="Instagram"
+      /></a>
+      <a href="https://www.tiktok.com/@rivers_edge_racing?_t=8dujULvjSwg&_r=1"
+        ><img src="../assets/images/tiktok.svg" alt="TikTok"
+      /></a>
+    </div>
   </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
 
 <style scoped>
-.read-the-docs {
-  color: #888;
+* {
+  box-sizing: border-box;
+}
+body {
+  margin: 0 !important;
+}
+.layout {
+  position: relative;
+  width: 100%;
+  height: 100%;
+}
+.info-container {
+  position: absolute;
+  top: 50%;
+  right: 0;
+  width: 85%;
+  max-height: 600px;
+  transform: translateY(-50%);
+  z-index: 2;
+  background: rgba(0, 0, 0, 0.5);
+  border-left: 5px solid rgba(255, 255, 255, 0.6);
+  display: flex;
+  overflow: hidden;
+  padding: 1em 0 1em 1em;
+  height: auto;
+}
+.text-container {
+  padding: 10px;
+  flex: 1;
+  /* overflow: hidden;  */
+}
+.image-container {
+  flex: 2;
+  overflow: hidden;
+}
+.image-container img {
+  width: 100%;
+  height: auto;
+  max-width: 100%;
+  display: block;
+}
+.text-container {
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  /* align-items: center;   
+  justify-content: center;  */
+  color: white;
+}
+h1 {
+  font-size: 2rem;
+  margin: 0 0 0.5em 0;
+}
+p {
+  margin: 0 0 0.5em 0;
+}
+.info {
+  padding: 20px;
+}
+.video-container {
+  width: 100%;
+  height: 100%;
+  z-index: 1;
+}
+.video-container::before {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: rgba(0, 0, 0, 0.9);
+  pointer-events: none;
+  z-index: 1;
+}
+video {
+  width: 100vw;
+  height: 100vh;
+  object-fit: cover;
+  z-index: 0;
+}
+.button-container {
+  margin-top: 1em;
+}
+.cta-button {
+  background: transparent;
+  border: 1px solid white;
+  border-radius: 10px;
+  color: white;
+  padding: 0.5em 3em;
+}
+.social-icons {
+  position: absolute;
+  bottom: 10px; /* Adjust as needed */
+  left: 50%; 
+  transform: translateX(-50%); /* Center the icons horizontally */
+  z-index: 2; /* To overlay the video but can adjust if it's not above or below as required */
+  display: flex;
+  gap: 20px; /* Space between the icons */
+  color: white;
+}
+.social-icons a img {
+  width: 32px; /* Adjust as needed */
+  height: auto;
+  color: white;
+}
+svg path {
+    fill: white;
 }
 </style>
